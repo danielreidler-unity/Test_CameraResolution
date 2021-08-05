c3c4ad1659f822be9b9cf7f19b8c95c65d33c6ff
Daniel Reidler <daniel.reidler@unity3d.com>
2021-08-05 17:22:15 +0200

    Unity project commit

---
+++ Assets/CamScale.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CamScale : MonoBehaviour
+{
+    public new Camera camera;
+    [Range(0.01F, 1.0F)]
+    public float renderScale = 1.0F;
+    public FilterMode filterMode = FilterMode.Bilinear;
+ 
+    private Rect originalRect;
+    private Rect scaledRect;
+ 
+    void OnDestroy ()
+    {
+        camera.rect = originalRect;
+    }
+ 
+    void OnPreRender ()
+    {
+        originalRect = camera.rect;
+        scaledRect.Set(originalRect.x, originalRect.y, originalRect.width * renderScale, originalRect.height * renderScale);
+        camera.rect = scaledRect;
+    }
+ 
+    void OnRenderImage (RenderTexture src, RenderTexture dest)
+    {
+        camera.rect = originalRect;
+        src.filterMode = filterMode;
+        Graphics.Blit(src, dest);
+    }
+}
---
+++ Assets/DynamicResolution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DynamicResolution : MonoBehaviour
+{
+    public Text screenText;
+    
+    [Range(0.1f, 1.0f)]
+    public float renderScale = 1.0f;
+
+    private float _maxResolutionScale = 1.0f;
+    private float _minResolutionScale = 0.1f;
+
+    private float _scale = 1.0f;
+
+    void Update()
+    {
+        float oldScale = _scale;
+        _scale = Mathf.Clamp((float)Math.Round(renderScale, 1), _minResolutionScale, _maxResolutionScale);
+
+        if (Math.Abs(_scale - oldScale) > 0.01f)
+        {
+            ScalableBufferManager.ResizeBuffers(_scale, _scale);
+        }
+        
+        // Debug on screen
+        screenText.text = $"Scale: {_scale:F3}\nResolution: " +
+                          $"{Mathf.Ceil(ScalableBufferManager.widthScaleFactor * Screen.currentResolution.width)}x" +
+                          $"{Mathf.Ceil(ScalableBufferManager.heightScaleFactor * Screen.currentResolution.height)}\n" +
+                          $"ScaleFactor: {ScalableBufferManager.widthScaleFactor:F3}x{ScalableBufferManager.heightScaleFactor:F3}";
+    }
+}
